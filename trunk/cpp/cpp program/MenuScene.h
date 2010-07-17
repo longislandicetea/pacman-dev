86e20877a91fe2bf9fd07dfa85343eebd8a2dd2a
yvetterowe1116 <yvetterowe1116@f51c43c4-b6b4-8dcf-abf3-865edae2f4e3>
2010-07-17 03:44:28 +0000

    add menu

---
+++ trunk/cpp/cpp program/MenuScene.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <hge.h>
+#include <hgefont.h>
+#include <hgegui.h>
+#include <hgesprite.h>
+#include <hgeresource.h>
+#include "IScene.h"
+
+class MenuScene : public IScene
+{
+public:
+	MenuScene();
+	virtual bool FrameFunc();
+	virtual bool RenderFunc();
+	~MenuScene();
+private:
+	hgeGUI *guiMenu;
+	HGE *hge;
+	HEFFECT snd;
+	HTEXTURE tex;
+	hgeQuad	 quad;
+	hgeSprite *spr;
+	hgeFont *fnt;
+	hgeResourceManager *manager;
+};
