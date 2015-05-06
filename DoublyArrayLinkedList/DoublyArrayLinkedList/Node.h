af6e0eb6894258c67f1585253df4ea9ceeb8d726
longislandicetea <haoluo3@illinois.edu>
2015-05-06 15:42:21 -0500

    delete redundant codes..

--- DoublyArrayLinkedList/DoublyArrayLinkedList/Node.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include<iostream>
-
-class Node
-{
-public:
-	int Diff;
-	int Entry;
-};
--- TreadBinaryTree/TreadBinaryTree/Main.cpp
+++
@@ -1,9 +0,0 @@
-#include"ThreadBinaryTree.h"
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	ThreadBinaryTree a;
-	return 0;
-}
